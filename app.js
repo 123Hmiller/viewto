b76a3c11bd1ebfddb0008036e67066f6cd624f84
Hunter <123hmiller@frhsd.com>
2023-02-17 10:09:35 -0500

    done

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ data() {
         url: 'http://www.thenetninja.co.uk',
         showBooks: true,
         books: [
-            { title: 'name of the wind', author: 'patrick rothfuss'},
-            { title: 'the way of kings', author: 'brandon sanderson'},
-            { title: 'the final empire', author: 'brandon sanderson'}
+            { title: 'name of the wind', author: 'patrick rothfuss', img: "assets/5.png", isFav: true },
+            { title: 'the way of kings', author: 'brandon sanderson',img: "assets/go.png", isFav: false },
+            { title: 'the final empire', author: 'brandon sanderson', img: "assets/six.png",isFav: true },
         ]
         // title: 'The Final Empire',
         // author: 'Brandon Sanderson',
